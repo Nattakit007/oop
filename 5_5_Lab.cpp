709ed675c437f42e39579822648a1bffb4323da6
Nattakit007 <npreesak@gmail.com>
2026-01-09 00:39:34 +0700

    Homework Done

---
+++ 5_5_Lab.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class prototype_calculator
+{
+public:
+    virtual int add(int x, int y) = 0;
+    virtual int subtract(int x, int y) = 0;
+    virtual int multiply(int x, int y) = 0;
+    virtual int sum(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10) = 0;
+};
+class int_calculator : public prototype_calculator
+{
+public:
+    int add(int x, int y) override
+    {
+        return x + y;
+    }
+    int subtract(int x, int y) override
+    {
+        return x - y;
+    }
+    int multiply(int x, int y) override
+    {
+        return x * y;
+    }
+    int sum(int x1, int x2 = 0, int x3 = 0, int x4 = 0, int x5 = 0, int x6 = 0, int x7 = 0, int x8 = 0, int x9 = 0, int x10 = 0) override
+    {
+        return x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+    }
+};
+
+int main()
+{
+
+    int_calculator cal;
+    cout<<fixed<<setprecision(2)<<endl;
+    cout << "--add--:--substract--:--multiply--" << endl;
+    cout << setw(5) << cal.add(6, 9) << "  : "
+         << setw(8) << cal.subtract(6, 9) << "    : "
+         << setw(6) << cal.multiply(6, 9) << endl;
+    cout << cal.add(6, 9) << " : " << cal.subtract(6, 9) << " : " << cal.multiply(6, 9) << endl;
+    cout << "---------------sum----------------" << endl;
+    cout << cal.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << "," << cal.sum(1, 2, 3, 4, 5, 6, 7, 8, 9) << "," << cal.sum(1, 2, 3, 4, 5, 6, 7, 8) << ",";
+    cout << cal.sum(1, 2, 3, 4, 5, 6, 7) << "," << cal.sum(1, 2, 3, 4, 5, 6) << "," << cal.sum(1, 2, 3, 4, 5) << "," << cal.sum(1, 2, 3, 4) << ",";
+    cout << cal.sum(1, 2, 3) << "," << cal.sum(1, 2) << "," << cal.sum(1) << endl;
+
+    return 0;
+}
---
+++ 5_6_Lab.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class prototype_calculator
+{
+public:
+    virtual double add(double x, double y) = 0;
+    virtual double subtract(double x, double y) = 0;
+    virtual double multiply(double x, double y) = 0;
+
+    virtual double sum(double x1, double x2, double x3, double x4, double x5, double x6, double x7, double x8, double x9, double x10) = 0;
+
+    virtual double avg(double x1, double x2, double x3, double x4, double x5) = 0;
+};
+class double_calculator : public prototype_calculator
+{
+public:
+    double add(double x, double y) override
+    {
+        return x + y;
+    }
+    double subtract(double x, double y) override
+    {
+        return x - y;
+    }
+    double multiply(double x, double y) override
+    {
+        return x * y;
+    }
+    double sum(double x1, double x2 = 0, double x3 = 0, double x4 = 0, double x5 = 0, double x6 = 0, double x7 = 0, double x8 = 0, double x9 = 0, double x10 = 0) override
+    {
+        return x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+    }
+    double avg(double x1, double x2 = 0, double x3 = 0, double x4 = 0, double x5 = 0) override
+    {
+        return (x1 + x2 + x3 + x4 + x5) / 5.0;
+    }
+};
+int main()
+{
+
+    double_calculator cal;
+    cout << fixed << setprecision(2) << endl;
+    cout << "--add--:--substract--:--multiply--" << endl;
+    cout << setw(5) << cal.add(6, 9) << "  : "
+         << setw(8) << cal.subtract(6, 9) << "    : "
+         << setw(6) << cal.multiply(6, 9) << endl;
+    cout << "sum" << endl;
+    cout << cal.sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << "," << cal.sum(1, 2, 3, 4, 5, 6, 7, 8, 9) << "," << cal.sum(1, 2, 3, 4, 5, 6, 7, 8) << ",";
+    cout << cal.sum(1, 2, 3, 4, 5, 6, 7) << "," << cal.sum(1, 2, 3, 4, 5, 6) << "," << cal.sum(1, 2, 3, 4, 5) << "," << cal.sum(1, 2, 3, 4) << ",";
+    cout << cal.sum(1, 2, 3) << "," << cal.sum(1, 2) << "," << cal.sum(1) << endl;
+
+    // special point
+    cout << "----------------avg--------------" << endl;
+    cout << cal.avg(1, 2, 3, 4, 5) << "," << cal.avg(1, 2, 3, 4) << "," << cal.avg(1, 2, 3) << "," << cal.avg(1, 2) << "," << cal.avg(1) << endl;
+
+    return 0;
+}
