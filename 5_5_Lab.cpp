6a6f5a3dfa92b24dc0246ae6bbf6dce96c910d20
Nattakit007 <npreesak@gmail.com>
2026-01-16 20:18:55 +0700

    oop lab 6

--- 5_5_Lab.cpp
+++ 5_5_Lab.cpp
@@ -14,19 +14,19 @@ public:
 class int_calculator : public prototype_calculator
 {
 public:
-    int add(int x, int y) 
+    int add(int x, int y)
     {
         return x + y;
     }
-    int subtract(int x, int y) 
+    int subtract(int x, int y)
     {
         return x - y;
     }
-    int multiply(int x, int y) 
+    int multiply(int x, int y)
     {
         return x * y;
     }
-    int sum(int x1, int x2 = 0, int x3 = 0, int x4 = 0, int x5 = 0, int x6 = 0, int x7 = 0, int x8 = 0, int x9 = 0, int x10 = 0) 
+    int sum(int x1, int x2 = 0, int x3 = 0, int x4 = 0, int x5 = 0, int x6 = 0, int x7 = 0, int x8 = 0, int x9 = 0, int x10 = 0)
     {
         return x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
     }
@@ -36,7 +36,7 @@ int main()
 {
 
     int_calculator cal;
-    cout<<fixed<<setprecision(2)<<endl;
+    cout << fixed << setprecision(2) << endl;
     cout << "--add--:--substract--:--multiply--" << endl;
     cout << setw(5) << cal.add(6, 9) << "  : "
          << setw(8) << cal.subtract(6, 9) << "    : "
