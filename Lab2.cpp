640dc46818bae57e0e2d06a9e98449a5f924886e
Nattakit007 <npreesak@gmail.com>
2026-01-21 01:22:40 +0700

    Lab2upd

--- Lab2.cpp
+++ Lab2.cpp
@@ -7,6 +7,7 @@ int main (){
     int inp;
     cout << " *** Multiplication table ***" << endl << "Enter a number (2-100) : ";
     cin >> inp;
+    if(inp <=100){
     for(int n = 1;n <= 12;n++){
         int sum = inp*n;
         cout << setw(2) << right << inp;
@@ -14,5 +15,9 @@ int main (){
         cout << setw(2) << left << n;
         cout << " = " << sum << endl;
     }
+}else{
+    cout<< "I said max is 100" << endl;
+}
+
     return 0;
 }
